3470f048652a884ba0aedf5c60c55a6a8bd41048
Ciara Nightingale <ciara.nightingale@sky.com>
2024-06-23 11:10:25 +0100

    different signing flow for zkSync

---
+++ script/SplitSignature.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { Script, console } from "forge-std/Script.sol";
+
+contract SplitSignature is Script {
+    function splitSignature(bytes memory sig)
+        internal
+        pure
+        returns (
+            uint8 v,
+            bytes32 r,
+            bytes32 s
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+    function run() external {
+        string memory sig = vm.readFile("signature.txt");
+        bytes memory sigBytes = vm.parseBytes(sig);
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(sigBytes);
+        console.log("v value:");
+        console.log(v);
+        console.log("r value:");
+        console.logBytes32(r);
+        console.log("s value:");
+        console.logBytes32(s);
+    }
+}
