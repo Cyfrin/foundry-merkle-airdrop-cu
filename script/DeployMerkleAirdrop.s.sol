8facd316836c9162dcb1275c35de740014b2eff4
Ciara Nightingale <ciara.nightingale@sky.com>
2024-08-05 12:54:24 +0100

    update root in deploy

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -7,7 +7,7 @@ import { BagelToken } from "../src/BagelToken.sol";
 import { console } from "forge-std/console.sol";
 
 contract DeployMerkleAirdrop is Script {
-    bytes32 public ROOT = 0x474d994c58e37b12085fdb7bc6bbcd046cf1907b90de3b7fb083cf3636c8ebfb;
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     // 4 users, 25 Bagel tokens each
     uint256 public AMOUNT_TO_TRANSFER = 4 * (25 * 1e18);
 
