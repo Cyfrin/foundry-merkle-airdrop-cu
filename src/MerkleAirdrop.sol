4e04a382e3310a53e706b5dc8f84f600962ca523
Ciara Nightingale <ciara.nightingale@sky.com>
2024-10-25 14:58:22 +0100

    cleanup comments and formatting

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -68,7 +68,7 @@ contract MerkleAirdrop is EIP712 {
         // Verify the merkle proof
         // calculate the leaf node hash
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
-        // verify the merkle proof (TODO: understand verify)
+        // verify the merkle proof
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
