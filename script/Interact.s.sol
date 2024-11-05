df2d83a188c470fde45fa8f164640c3565af3bc7
Luigi <luigi@umbrellavalidator.com>
2024-11-05 00:36:21 -0800

    Fix typos and trim whitespace

--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -12,7 +12,7 @@ contract ClaimAirdrop is Script {
     bytes32 private constant PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
     bytes32 private constant PROOF_TWO = 0x46f4c7c1c21e8a90c03949beda51d2d02d1ec75b55dd97a999d3edbafa5a1e2f;
     bytes32[] private proof = [PROOF_ONE, PROOF_TWO];
-    
+
     // the signature will change every time you redeploy the airdrop contract!
     bytes private SIGNATURE = hex"fbd2270e6f23fb5fe9248480c0f4be8a4e9bd77c3ad0b1333cc60b5debc511602a2a06c24085d8d7c038bad84edc53664c8ce0346caeaa3570afec0e61144dc11c";
 
@@ -30,7 +30,7 @@ contract ClaimAirdrop is Script {
     function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
         if (sig.length != 65) {
             revert __ClaimAirdropScript__InvalidSignatureLength();
-        }   
+        }
         assembly {
             r := mload(add(sig, 32))
             s := mload(add(sig, 64))
